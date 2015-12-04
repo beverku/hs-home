a01c00102d5f8e4ab748a61f334df07dc83b2971
rbever <rbever@netapp.com>
2015-12-04 11:25:32 -0700

    Comment out mbp export

--- repos/hs-mbp17/home/.bashrc.thismachine.first.bash
+++ repos/hs-mbp17/home/.bashrc.thismachine.first.bash
@@ -1,4 +1,4 @@
-export PATH=$PATH:$GRADLE_HOME/bin:$PHANTOM_HOME/bin
+#export PATH=$PATH:$GRADLE_HOME/bin:$PHANTOM_HOME/bin
 
 #JAVA_HOME set in ~/.launchd.conf
 #export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
